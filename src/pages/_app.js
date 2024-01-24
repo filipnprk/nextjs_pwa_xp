44545e590e191905af99bc34c630d4bc29145585
filipnprk <147948206+filipnprk@users.noreply.github.com>
2024-01-24 23:09:55 +0000

    service worker registration added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,20 @@ import Head from 'next/head';
 import { Gtm, GTM_ID } from '../components/Gtm';
 
 function App({ Component, pageProps }){
+
+  useEffect(() => {
+    //register the service worker in production 
+    if(process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch(registrationError => {
+          console.log('Service Worker registration failed: ', registrationError);
+        });
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -16,6 +30,7 @@ function App({ Component, pageProps }){
           style={{ display: 'none', visibility: 'hidden' }}
         ></iframe>
       </noscript>
+    
       <Component {...pageProps} />
     </>
   );
